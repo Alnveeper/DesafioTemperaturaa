7b0322fd419e029b9242246c07263d7dbf3e4554
Alnveeper <allan.pereira@meninashoes.com.br>
2025-02-23 17:41:37 -0300

    Update do Desafio Temperatura

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double temperaturaEmCelsius = 30.4;
+        double temperaturaFarenheit = (temperaturaEmCelsius * 1.8) + 32;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        /*System.out.println("A temperatura em Farenheit é: " + temperaturaFarenheit);*/
+
+        String mensagem = String.format("A temperatura de %f Celsius é equivalente a %f Farenheit", temperaturaEmCelsius, temperaturaFarenheit);
+        System.out.println(mensagem);
+
+        /*converter para int*/
+        int temperaturaEmFarenheit = (int) temperaturaFarenheit;
+        System.out.println("\n");
+        System.out.println("A temperatura em Farenheit inteira é: " + temperaturaEmFarenheit);
+
+
     }
 }
